5401e3692b24575756657f90d312eed2352ec1ad
Felix Warmuth <felixwarmuth@mailbox.org>
2025-08-02 09:11:04 +0200

    remove old things

--- src/cameracommander/__init__.py
+++
@@ -1,5 +0,0 @@
-from loguru import logger
-import sys
-
-logger.add(sys.stderr, format="{time} {level} {extra[file_name]}:{line} {message}", filter=__name__, level="INFO")
-logger = logger.patch(lambda record: record["extra"].update(file_name=record["name"]))
