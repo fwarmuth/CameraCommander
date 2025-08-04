d64fde3ddf73639df76d77d9425bef52f5e3b5ab
Felix Warmuth <felixwarmuth@mailbox.org>
2025-08-04 19:20:35 +0200

    Remove rudiments

--- src/advanced_live_view/__init__.py
+++
@@ -1,4 +0,0 @@
-from .camera import initialize_camera, close_camera
-from .timelapse_config_ui import create_gradio_interface
-
-__all__ = ["initialize_camera", "close_camera", "create_gradio_interface"]
